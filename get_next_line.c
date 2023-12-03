006f5df1798fcc37cb23a70730a2a25d29a42ad9
igama <igama@student.42.rio>
2023-12-02 22:34:59 -0300

    Really started the project, and had my first thoughts about it

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,17 @@
 /*   By: igama <igama@student.42.rio>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:54:21 by igama             #+#    #+#             */
-/*   Updated: 2023/11/30 15:54:22 by igama            ###   ########.fr       */
+/*   Updated: 2023/12/02 19:46:10 by igama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <fcntl.h>
 
+char	*get_next_line(int fd)
+{
+	fd = foo.txt;
+	if (fd < 0)
+		return ("error");
+	open(fd, O_RDONLY | O_EXCL);
+}
